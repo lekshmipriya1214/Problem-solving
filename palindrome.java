85584505583ea2180cdb9a2401e2a520fe546222
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-11-28 22:21:49 +0530

    Palindrome of a string

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String word =scan.nextLine();
+        scan.close();
+        String palindrome="";
+        for(int i=word.length()-1;i>=0;i--){
+            palindrome+=word.charAt(i);
+
+        }if(word.equals(palindrome)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a palindrome");
+        }
+        
+
+    }
+}
