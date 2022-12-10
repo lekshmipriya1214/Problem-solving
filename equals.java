6e6b5c720a3d58b004aac62a407a4a85feb8d438
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-12-10 23:22:23 +0530

    Integer equality

---
+++ equals.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Equals {
+    public static void main(String[] args){
+                Scanner scan=new Scanner(System.in);
+                int num1=scan.nextInt();
+                int num2=scan.nextInt();
+                scan.close();
+                System.out.println(num1==num2);
+               
+    }
+}
