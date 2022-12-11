7afb30ec9e5d74709addf19ad9baf8797250533b
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-12-11 20:31:45 +0530

    complete ascii values

---
+++ fullascii.java
@@ -0,0 +1,7 @@
+public class fullascii {
+    public static void main(String[] args){
+    for(int i=0;i<=255;i++){
+        System.out.println("The ascii value of "+ (char)i+" is "+ i );
+    }
+}
+}
