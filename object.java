265ea925d00169d15415ccb7d4d6e4601f923ff1
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-12-11 19:30:38 +0530

    Creating object

---
+++ object.java
@@ -0,0 +1,9 @@
+ class Object {
+    public void show(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args){
+    Object obj1=new Object();
+    obj1.show();
+    }
+}
