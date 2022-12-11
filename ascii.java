373430b1e7e262a49cb06b85f9161312ac19fbd8
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-12-11 20:30:46 +0530

    Ascii value

---
+++ ascii.java
@@ -0,0 +1,26 @@
+class ascii {
+    public static void main(String[] args){
+        int a='a';
+        int b='b';
+
+        System.out.println(a);
+        System.out.println(b);
+
+    char ch1='c';
+    char ch2='d';
+
+    int asciivalue=ch1;
+    int asciivalue1=ch2;
+
+    System.out.println(asciivalue);
+    System.out.println(asciivalue1);
+
+        int a1=(int)'a';
+        int a2=(int)'b';
+
+        System.out.println(a1);
+        System.out.println(a2);
+      
+    }
+    
+}
