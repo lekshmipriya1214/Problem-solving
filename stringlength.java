cdb3dce1c9b7d621ad1cf01fa98aab966d5d5368
Lekshmi priya.S <72740768+lekshmipriya1214@users.noreply.github.com>
2022-12-12 20:45:26 +0530

    Length of a string

---
+++ stringlength.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+class Stringlength{
+    public static void main(String[] args){
+                Scanner scan=new Scanner(System.in);
+                String str=scan.nextLine();
+                scan.close();
+                System.out.println("The length of the string is "+str.length());
+    }
+}
